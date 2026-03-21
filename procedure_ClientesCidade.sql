2c0c9c0efd677ec0bdb83c75a9a10ec50373c779
Vinicius Campos <viniciuscampos131209@gmail.com>
2026-03-21 09:47:02 -0300

    correção procedure ClientesCidade

--- procedure_ClientesCidade.sql
+++ procedure_ClientesCidade.sql
@@ -3,10 +3,12 @@ create procedure ClientesCidade
 	@idade int
 as
 begin
-	select cidade, nome, 2026 - year(datanascimento) as idade
+	select nome, datanascimento, 2026 - year(datanascimento) as idade
 	from clientes
 	where cidade = @cidade
 	and 2026 - year(datanascimento) = @idade;
 end;
+
+-- se precisar implementar mais tarde: format(Datanascimento, 'dd/MM/yyyy') as DataNascimento
 
 exec ClientesCidade 'Sorocaba', '41';
